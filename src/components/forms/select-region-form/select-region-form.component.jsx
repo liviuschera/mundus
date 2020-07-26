62e844968a28b87ab5c5a2118bc7e0d903d9fe4b
liviuschera <liviu.schera@gmail.com>
2020-07-26 11:35:02 +1000

    implemented a reusable data fetching hook and using it to pull out data about countries

--- src/components/forms/select-region-form/select-region-form.component.jsx
+++ src/components/forms/select-region-form/select-region-form.component.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import { FormWrapper } from '../search-form/search-form.styles';
-// import { FormWrapper } from './select-form.styles';
+// import { FormWrapper } from '../search-form/search-form.styles';
+import { FormWrapper } from './select-region-form.styles';
 import Select from '../select/select.component';
 
 export default function SelectForm({ onSelectChange, region }) {
   return (
     <FormWrapper>
       <Select
-        onSelect={onSelectChange}
-        value={region}
+        onChange={onSelectChange}
+        // value={region}
         region={region}
         name="select-region"
       ></Select>
--- src/pages/homepage.component.jsx
+++ src/pages/homepage.component.jsx
@@ -4,37 +4,29 @@ import { HomepageWrapper, FormSection } from './homepage.styles';
 
 import connectToAPI from '../utils/connect-to-api';
 import SearchForm from '../components/forms/search-form/search-form.component';
-import SelectForm from '../components/forms/select-form/select-form.component';
+import SelectForm from '../components/forms/select-region-form/select-region-form.component';
 import CountryCardList from '../components/country-card-list/country-card-list.component';
 
 export default function Homepage(params) {
-  const [countries, setCountries] = useState([]);
+  // const [countries, setCountries] = useState([]);
   const [selectRegion, setSelectRegion] = useState('');
   const [inputCountry, setInputCountry] = useState('');
 
-  async function getCountries() {
-    (async function () {
-      const response = await connectToAPI(
-        'https://restcountries.eu/rest/v2/all',
-        'GET'
-      );
-      setCountries(response);
-      console.log(response);
-    })();
-  }
+  const useFetch = (url) => {
+    const [data, setData] = useState(null);
 
-  // function getRegion(selectRegion) {
-  //   const response = fetch(
-  //     `https://restcountries.eu/rest/v2/region/${selectRegion}`
-  //   );
-  //   return response;
+    async function fetchData() {
+      const response = await fetch(url);
+      const json = await response.json();
+      setData(json);
+    }
 
-  //   // (async function (selectRegion) {
-  //   //   setSelectRegion(response);
-  //   //   console.log(response);
-  //   // })();
-  // }
-
+    useEffect(() => {
+      fetchData();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [url]);
+    return data;
+  };
   function onInputChange(event) {
     setInputCountry(event.target.value);
   }
@@ -43,19 +35,29 @@ export default function Homepage(params) {
     setSelectRegion(event.target.value);
   }
 
-  useEffect(() => {
-    getCountries();
-  }, []);
+  const countries = useFetch('https://restcountries.eu/rest/v2/all');
+  console.log(countries);
 
   // useEffect(() => {
-  //   (async () => {
+  //   async function getCountries() {
+  //     const response = await fetch('https://restcountries.eu/rest/v2/all');
+  //     const data = response.json();
+  //     setCountries(data);
+  //     console.log(data);
+  //   }
+  //   getCountries();
+  // }, []);
+
+  // useEffect((selectRegion) => {
+  //   async function getRegion(selectRegion) {
   //     const response = await fetch(
   //       `https://restcountries.eu/rest/v2/region/${selectRegion}`
   //     );
-  //     const data = await response.json();
+  //     const data = response.json();
   //     setSelectRegion(data);
-  //   })();
-  // }, [selectRegion]);
+  //   }
+  //   getRegion(selectRegion);
+  // }, []);
 
   return (
     <HomepageWrapper>
@@ -63,7 +65,11 @@ export default function Homepage(params) {
         <SearchForm onInputChange={onInputChange} countries={countries} />
         {/* <SelectForm onSelectChange={onSelectChange} region={selectRegion} /> */}
       </FormSection>
-      <CountryCardList countries={countries} inputCountry={inputCountry} />
+      {countries ? (
+        <CountryCardList countries={countries} inputCountry={inputCountry} />
+      ) : (
+        <h1>Loading...</h1>
+      )}
     </HomepageWrapper>
   );
 }
